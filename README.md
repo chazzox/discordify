387fac7bb2c871f89d7902c099fec72bc9ce234b
chazzox <caylott@outlook.com>
2021-10-07 16:56:32 +0100

    updated readme and create plguin file with formatter config

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# SPOTIFY-DISCORD
- 
+# spotifyDiscord
+
 ## About
 
- - This is a plugin for better discord that gives you pause/queue and playback control
+-   A plugin for better discord that gives you pause/queue and playback control
---
+++ SpotifyDiscord.plugin.js
@@ -0,0 +1,18 @@
+/**
+ * @name SpotifyDiscord
+ * @author chazzox, pinpal test
+ * @description Spotify but inside discord
+ * @version 0.0.1
+ * @website https://github.com/PINPAL/spotifyDiscord#readme
+ * @source https://github.com/PINPAL/spotifyDiscord
+ */
+
+module.exports = class SpotifyDiscord {
+	load() {
+		console.log('loaded SpotifyDiscord plugin');
+	}
+	start() {
+		BdApi.alert('wagwan', 'uwu');
+	}
+	stop() {}
+};
