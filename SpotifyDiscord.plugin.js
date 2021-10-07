6ffd8b85ae3baf15998c8bac3ccb2d095f2278a0
chazzox <caylott@outlook.com>
2021-10-08 00:47:13 +0100

    fleshed out components more

--- SpotifyDiscord.plugin.js
+++ SpotifyDiscord.plugin.js
@@ -7,18 +7,66 @@
  * @source https://github.com/PINPAL/spotifyDiscord
  */
 
+// container ids
+const sidebarContainerId = 'container-2lgZY8';
+const toolbarContainerId = 'toolbar-1t6TWx';
+
+// wrapper ids
+const sidebarId = 'discordSpotifySidebar';
+const toolBarId = 'discordSpotifyToolbar';
+
+let hide = false;
+
+function addSideBar() {
+	const reactTarget = document.createElement('div');
+	reactTarget.id = sidebarId;
+	document.getElementsByClassName(sidebarContainerId)[0].append(reactTarget);
+	const reactTargetDom = document.getElementById(sidebarId);
+	BdApi.ReactDOM.render(
+		BdApi.React.createElement(
+			BdApi.React.Fragment,
+			null,
+			!hide ? BdApi.React.createElement('h1', null, 'hello world') : null
+		),
+		reactTargetDom
+	);
+}
+
+function addToolbar() {
+	const reactToolbarEl = document.createElement('div');
+	reactToolbarEl.id = toolBarId;
+	document.getElementsByClassName(toolbarContainerId)[0].append(reactToolbarEl);
+	const reactTargetDom = document.getElementById(toolBarId);
+	BdApi.ReactDOM.render(
+		BdApi.React.createElement(
+			'button',
+			{
+				onClick: () => {
+					hide = false;
+				}
+			},
+			'toggle'
+		),
+		reactTargetDom
+	);
+}
+
+function callback() {
+	BdApi.onRemoved(document.getElementById(toolBarId), () => {
+		addToolbar();
+		callback();
+	});
+}
+
 module.exports = class SpotifyDiscord {
 	load() {}
 	start() {
-		BdApi.alert('wagwan', 'uwu\nthis is a test');
-		const reactTarget = document.createElement('div');
-		reactTarget.id = 'discordSpotifyTarget';
-		document.getElementsByClassName('container-2lgZY8')[0].append(reactTarget);
-		const reactTargetDom = document.getElementById('discordSpotifyTarget');
-		BdApi.ReactDOM.render(BdApi.React.createElement('p', null, 'hello world from react'), reactTargetDom);
+		addSideBar();
+		addToolbar();
+		callback();
 	}
 	stop() {
-		const reactTargetDom = document.getElementById('discordSpotifyTarget');
-		reactTargetDom.remove();
+		document.getElementById(sidebarId)?.remove();
+		document.getElementById(toolBarId)?.remove();
 	}
 };
