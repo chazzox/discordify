be62e17775173fbfe62df83ef4441c28cad0c309
chazzox <caylott@outlook.com>
2021-10-07 22:16:14 +0100

    added hello world text

--- SpotifyDiscord.plugin.js
+++ SpotifyDiscord.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name SpotifyDiscord
- * @author chazzox, pinpal test
+ * @author Pinpal and Chazzox
  * @description Spotify but inside discord
  * @version 0.0.1
  * @website https://github.com/PINPAL/spotifyDiscord#readme
@@ -8,11 +8,14 @@
  */
 
 module.exports = class SpotifyDiscord {
-	load() {
-		console.log('loaded SpotifyDiscord plugin');
-	}
+	load() {}
 	start() {
-		BdApi.alert('wagwan', 'uwu');
+		BdApi.alert('wagwan', 'uwu\nthis is a test');
+		const reactTarget = document.createElement('div');
+		reactTarget.id = 'discordSpotifyTarget';
+		document.getElementsByClassName('container-2lgZY8')[0].append(reactTarget);
+		const reactTargetDom = document.getElementById('discordSpotifyTarget');
+		BdApi.ReactDOM.render(BdApi.React.createElement('p', null, 'hello world from react'), reactTargetDom);
 	}
 	stop() {}
 };
