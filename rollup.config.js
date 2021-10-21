3b221c2088dd7bfbb6dd498eb0300fa2f711dc70
chazzox <42206116+chazzox@users.noreply.github.com>
2021-10-21 04:25:58 +0100

    Builds to better discord plugin location as well as plugin folder now

--- rollup.config.js
+++ rollup.config.js
@@ -4,21 +4,47 @@ import { terser } from 'rollup-plugin-terser';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import styles from 'rollup-plugin-styles';
 import { name } from './package.json';
+import os from 'os';
+import path from 'path';
 
-export default defineConfig({
-	input: 'src/discordify.jsx',
-	output: {
-		file: 'plugin/discordify.plugin.js',
-		format: 'cjs',
-		banner: `/**
+function GetBetterDiscordPath() {
+	switch (os.platform()) {
+		case 'darwin':
+			return [process.env.HOME, 'Library/Application Support/BetterDiscord/plugins/'];
+		case 'win32':
+			return [process.env.HOME, 'AppData/Roaming/BetterDiscord/plugins/'];
+		default:
+			throw Error(
+				'Platform not implemented, please submit an issue at https://github.com/chazzox/discordify/issues or make a pr :)'
+			);
+	}
+}
+
+const meta = `
+/**
  * @name discordify
  * @author PINPAL#5245 and chazzox#1001
  * @description Spotify but inside discord
  * @version 0.0.1
  * @website https://github.com/chazzox/discordify#readme
  * @source https://github.com/chazzox/discordify
- */`
-	},
+ */
+`;
+
+export default defineConfig({
+	input: 'src/discordify.jsx',
+	output: [
+		{
+			file: 'plugin/discordify.plugin.js',
+			format: 'cjs',
+			banner: meta
+		},
+		{
+			file: path.join(...GetBetterDiscordPath(), 'discordify.plugin.js'),
+			format: 'cjs',
+			banner: meta
+		}
+	],
 	plugins: [
 		nodeResolve(),
 		styles({
@@ -39,7 +65,7 @@ export default defineConfig({
 			mangle: {},
 			parse: {},
 			rename: {},
-			format: { comments: '/@name/' }
+			format: {}
 		})
 	]
 });
