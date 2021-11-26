60f339bbf1127defd7b7139bcad4be697b992001
chazzox <caylott@outlook.com>
2021-11-26 20:41:02 +0000

    Added monkey patch for toggle button as well as react portal for sidebar (not persistent yet)

--- src/discordify.jsx
+++ src/discordify.jsx
@@ -1,69 +1,42 @@
-import * as spotifyApi from './spotifyUtils';
 import './discordify.scss';
 
 // BdApi stuff that we use
 const { React, ReactDOM } = BdApi;
 
-// discord spotify code :)
-const SpotifyTrackUtils = BdApi.findModuleByProps('getActiveSocketAndDevice');
-const SpotifyUtils = BdApi.findModuleByProps('SpotifyAPI');
-
-const sidebarContainerClass = 'content-yTz4x3';
-const sidebarWrapperId = 'sidebarWrapper';
+const sidebarContainerClass = 'container-2lgZY8';
 
 function App() {
 	const [isHidden, setIsHidden] = React.useState(true);
-	const [accessToken, setAccessToken] = React.useState('');
-	const [userInfo, setUserInfo] = React.useState({});
-	const [playlists, setPlaylists] = React.useState([]);
-	const [showPlaylists, setShowPlaylists] = React.useState(false);
-
-	React.useEffect(() => {
-		const accountId = SpotifyTrackUtils?.getActiveSocketAndDevice()?.socket?.accountId;
-		accountId && SpotifyUtils.getAccessToken(accountId).then((res) => setAccessToken(res.body.access_token));
-	}, []);
-
-	React.useEffect(() => {
-		if (accessToken) {
-			spotifyApi.getUserInfo(accessToken).then((info) => setUserInfo(info));
-			spotifyApi.getPlaylists(accessToken).then((res) => setPlaylists(res));
-		}
-	}, [accessToken]);
 
 	return (
 		<>
-			<button onClick={() => setIsHidden(!isHidden)}>toggle</button>
-			{/* {ReactDOM.createPortal(
-				<>
-					{!isHidden && (
-						<>
-							{userInfo.display_name && <p>Fuck off {userInfo.display_name}</p>}
-							<button onClick={() => spotifyApi.previous(accessToken)}>backwards</button>
-							<button onClick={() => spotifyApi.togglePlay(accessToken)}>play/pause</button>
-							<button onClick={() => spotifyApi.next(accessToken)}>forwards</button>
-							<button onClick={() => setShowPlaylists(!showPlaylists)}>toggle playlists</button>
-							{showPlaylists && (
-								<div
-									id="playlistContainer"
-									style={{ width: '400px', overflowY: 'scroll', userSelect: 'text', height: '400px' }}
-								>
-									{playlists.map((playlist) => (
-										<p>{JSON.stringify(playlist)}</p>
-									))}
-								</div>
-							)}
-						</>
-					)}
-				</>,
-				document.getElementById(sidebarWrapperId)
-			)} */}
+			<button onClick={() => setIsHidden(!isHidden)}>{isHidden ? 'Open' : 'Close'} Discordify</button>
+			<Sidebar isHidden={isHidden} />
 		</>
 	);
 }
 
+const Sidebar = ({ isHidden }) => {
+	const container = document.createElement('div');
+	container.id = 'discordSpotifySidebar';
+	React.useEffect(() => {
+		console.log('mounted');
+		document.getElementsByClassName(sidebarContainerClass)[0].appendChild(container);
+		return () => {
+			console.log('unmounted');
+			document.getElementsByClassName(sidebarContainerClass)[0].removeChild(container);
+		};
+	}, [isHidden]);
+
+	return <>{ReactDOM.createPortal(<>{!isHidden && <h1>test</h1>}</>, container)}</>;
+};
+
 module.exports = class SpotifyDiscord {
 	patchedHeader = false;
 	cancel_patch_header() {}
+	load() {
+		this.patch();
+	}
 	start() {
 		this.patch();
 	}
@@ -72,10 +45,6 @@ module.exports = class SpotifyDiscord {
 	}
 	onSwitch() {
 		this.patch();
-		const Container = document.getElementsByClassName(sidebarContainerClass)[0];
-		if (Container && !document.getElementById(sidebarWrapperId)) {
-			console.log('need to create');
-		}
 	}
 	patch() {
 		const HeaderBarContainer = BdApi.findModuleByDisplayName('HeaderBarContainer')?.prototype;
