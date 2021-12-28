732b3d3ceb38afc1f48c103f975bcbfa3c2118a0
chazzox <caylott@outlook.com>
2021-12-28 21:45:52 +0000

    rank code that lets you toggle mute

--- src/components/volume.tsx
+++ src/components/volume.tsx
@@ -3,20 +3,27 @@ import React from 'react';
 import { Mute } from '@components/navbarIcons';
 
 const Volume: React.FC = () => {
-	const [volume, setVolume] = React.useState('65');
+	const [volume, setVolume] = React.useState(['65']);
 
 	return (
 		<div id="volumeLevel" className="progressBarRow row">
 			<button id="muteBtn">
-				<Mute onClick={() => setVolume('0')} />
+				<Mute
+					onClick={() =>
+						setVolume((prev) => {
+							// @Notice This is fucking rank please refactor soon.
+							return prev[0] == '0' ? [prev[1]] : ['0', prev[0]];
+						})
+					}
+				/>
 			</button>
 			<input
 				type="range"
 				className="progress"
 				min="0"
 				max="100"
-				value={volume}
-				onChange={(e) => setVolume(e.target.value)}
+				value={volume[0]}
+				onChange={(e) => setVolume([e.target.value, volume[0]])}
 			/>
 		</div>
 	);
